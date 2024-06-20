4ab3f9beb01d354149f77bb55829f5dee85ea16a
asheessharma <80744753+asheessharma@users.noreply.github.com>
2024-06-20 16:50:25 +0530

    Time: 3 ms (80.23%), Space: 44.5 MB (90.55%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,23 +1,46 @@
 class Solution {
     public String reverseVowels(String s) {
-        ArrayList<Character> list = new ArrayList<>();
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-            if(ch == 'a' || ch == 'e'|| ch == 'i'|| ch == 'o'|| ch == 'u'|| ch == 'A'|| ch == 'E'|| ch == 'I'|| ch == 'O'|| ch == 'U'){
-                list.add(ch);
+        // ArrayList<arr[i]aracter> list = new ArrayList<>();
+        // for(int i = 0 ; i < s.length() ; i++){
+        //     arr[i]ar arr[i] = s.arr[i]arAt(i);
+        //     if(arr[i] == 'a' || arr[i] == 'e'|| arr[i] == 'i'|| arr[i] == 'o'|| arr[i] == 'u'|| arr[i] == 'A'|| arr[i] == 'E'|| arr[i] == 'I'|| arr[i] == 'O'|| arr[i] == 'U'){
+        //         list.add(arr[i]);
+        //     }
+        // }
+        // String temp="";
+        // int len = list.size()-1;
+        // for(int j = 0 ; j < s.length() ; j++){
+        //     arr[i]ar arr[i] = s.arr[i]arAt(j);
+        //     if(arr[i] == 'a' || arr[i] == 'e'|| arr[i] == 'i'|| arr[i] == 'o'|| arr[i] == 'u'|| arr[i] == 'A'|| arr[i] == 'E'|| arr[i] == 'I'|| arr[i] == 'O'|| arr[i] == 'U'){
+        //         temp+=list.get(len--);
+        //     }
+        //     else{
+        //         temp+=arr[i];
+        //     }
+        // }
+        // return temp;
+
+        Set<Character> set = new HashSet<>();
+        char [] arr = s.toCharArray();
+        set.add('a');set.add('e');set.add('i');set.add('o');set.add('u');set.add('A');set.add('E');set.add('I');set.add('O');set.add('U');
+        int i = 0 ;
+        int j = s.length() - 1;
+        while(i< j){
+            while(i<j && !set.contains(arr[i])){
+                i++;
             }
+            while( i<j && !set.contains(arr[j])){
+                j--;
+            }
+            if(i<j){
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+
         }
-        String temp="";
-        int len = list.size()-1;
-        for(int j = 0 ; j < s.length() ; j++){
-            char ch = s.charAt(j);
-            if(ch == 'a' || ch == 'e'|| ch == 'i'|| ch == 'o'|| ch == 'u'|| ch == 'A'|| ch == 'E'|| ch == 'I'|| ch == 'O'|| ch == 'U'){
-                temp+=list.get(len--);
-            }
-            else{
-                temp+=ch;
-            }
-        }
-        return temp;
+        return new String(arr);
     }
 }
