cf4f01d588b10113cf72248bceddae74c5d7630c
asheessharma <80744753+asheessharma@users.noreply.github.com>
2024-06-23 17:28:55 +0530

    Time: 38 ms (22.91%), Space: 55.4 MB (86.38%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,24 +1,39 @@
+import java.util.*;
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int i = 0;
-        int j = nums.length - 1;
-        int opers = 0;
-        while(i<j){
-            int sum = nums[i]+nums[j];
-            if(sum==k){
-                opers++;
-                i++;
-                j--;
+        // Arrays.sort(nums);
+        // int i = 0;
+        // int j = nums.length - 1;
+        // int opers = 0;
+        // while(i<j){
+        //     int sum = nums[i]+nums[j];
+        //     if(sum==k){
+        //         opers++;
+        //         i++;
+        //         j--;
+        //     }
+        //     else if(sum < k){
+        //         i++;
+        //     }
+        //     else {
+        //         j--;
+        //     }
+        // }
+        // return opers;
+        int op = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int j = 0; j < nums.length; j++){
+            int compliment = k-nums[j];
+            if(map.getOrDefault(compliment , 0) > 0){
+                op++;
+                map.put(compliment, map.get(compliment)-1);
             }
-            else if(sum < k){
-                i++;
+            else{
+                map.put(nums[j], map.getOrDefault(nums[j],0)+1);
             }
-            else {
-                j--;
-            }
+            
         }
-        return opers;
+        return op;
 
     }
 }
