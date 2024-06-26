1f26eacc3582c64bee76827080872168102849e2
𝑨𝑺𝑯𝑰𝑺𝑯 <80744753+asheessharma@users.noreply.github.com>
2024-06-26 17:34:14 +0530

    Time: 1 ms (94.62%), Space: 45.7 MB (23.67%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -7,10 +7,11 @@ class Solution {
         left [0] = nums[0];
         for(int i = 1; i < nums.length; i++){
             left[i] = nums[i] + left[i-1];
+            right[n-i] = nums[n-i] + right[n-i+1];
         } 
-        for(int i = n-1 ; i >=0 ;i--){
-            right[i] = nums[i] + right[i+1];
-        }
+        // for(int i = n-1 ; i >=0 ;i--){
+        //     right[i] = nums[i] + right[i+1];
+        // }
         
         for(int j = 0; j < nums.length;j++){
             if(left[j] == right[j]){
